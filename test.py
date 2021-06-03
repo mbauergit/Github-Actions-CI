dfce171dd14cd536d9b219e9b5809351bf11f14d
Markus Bauer <ma88bauer@gmail.com>
2021-06-02 19:27:46 -0700

    mul and div tests added

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ class TestCase(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(example.sub(3, 1), 2)
 
+    def test_mul(self):
+        self.assertEqual(example.sub(3, 4), 12)
+
+    def test_div(self):
+        self.assertEqual(example.sub(6, 3), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
